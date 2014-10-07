220d5371164c7cb7dca1ac1861b27f5de1a0dc25
vincent rhino <vincent.rhino@smile.fr>
2014-10-07 12:16:53 +0200

    entity api

--- src/Virhi/RestApiDoctrineBundle/Controller/EntityController.php
+++ src/Virhi/RestApiDoctrineBundle/Controller/EntityController.php
@@ -11,7 +11,10 @@ namespace Virhi\RestApiDoctrineBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Alterway\Bundle\RestHalBundle\Response\HalResponse;
 use Virhi\RestApiDoctrineBundle\Api\Factory\ListEntityResourceFactory;
+use Virhi\RestApiDoctrineBundle\Api\Factory\EntityResourceFactory;
 use Virhi\RestApiDoctrineBundle\Api\Context\ListEntityContext;
+use Virhi\RestApiDoctrineBundle\Api\Context\EntityContext;
+use Doctrine\ORM\AbstractQuery;
 
 class EntityController extends Controller
 {
@@ -21,13 +24,31 @@ class EntityController extends Controller
 
         $qb = $doctrine->getEntityManager('default')->createQueryBuilder();
         $qb->select('x')
-             ->from('VirhiSymfonyDomainBundle:' .ucfirst($name), 'x');
+            ->from('VirhiSymfonyDomainBundle:' .ucfirst($name), 'x');
 
         $entitys = $qb->getQuery()->getArrayResult();
 
         $context  = new ListEntityContext($entitys, $this->get('router'));
         $resource = ListEntityResourceFactory::buildResource($context);
-        $reponse    = new HalResponse($resource);
+        $reponse  = new HalResponse($resource);
+        return $reponse;
+    }
+
+    public function entityAction($name, $id)
+    {
+        $doctrine = $this->get("doctrine");
+
+        $qb = $doctrine->getEntityManager('default')->createQueryBuilder();
+        $qb->select('x')
+            ->from('VirhiSymfonyDomainBundle:' .ucfirst($name), 'x')
+            ->where('x.id = :id')
+            ->setParameter('id', $id)
+        ;
+
+        $entity  = $qb->getQuery()->getSingleResult(AbstractQuery::HYDRATE_ARRAY);
+        $context  = new EntityContext($entity, $this->get('router'));
+        $resource = EntityResourceFactory::buildResource($context);
+        $reponse  = new HalResponse($resource);
         return $reponse;
     }
 }
